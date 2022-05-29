5ee0602f1c6645d49d30ab7b276d8f0455b30862
Doubleneck <antti.vuorenmaa@kolumbus.fi>
2022-05-29 20:40:48 +0300

    teht 1.8

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -1,26 +1,33 @@
 import { useState } from 'react'
-
+const Statistics = (props) => {
+  const all = props.bad+props.neutral+props.good
+  const average = (props.good-props.bad)/all
+  const positive = 100*props.good/all
+  return ( 
+    <div>
+      <h1>statistics</h1>
+      <div>good {props.good}</div>
+      <div>neutral {props.neutral}</div>
+      <div>bad {props.bad}</div>
+      <div>all {all}</div>
+      <div>average {average}</div>
+      <div>positive {positive} %</div>
+    </div>   
+  )
+}
 const App = () => {
   // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const all = bad+neutral+good
-  const average = (good-bad)/all
-  const positive = 100*good/all
+  
   return (
     <div>
       <h1>give feedback</h1>
       <button onClick={() => setGood(good + 1)}>good</button>
       <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
       <button onClick={() => setBad(bad + 1)}>bad</button>
-      <h1>statistics</h1>
-      <div>good {good}</div>
-      <div>neutral {neutral}</div>
-      <div>bad {bad}</div>
-      <div>all {all}</div>
-      <div>average {average}</div>
-      <div>positive {positive} %</div>
+      <Statistics good={good} neutral={neutral} bad={bad}/>
     </div>
   )
 }
