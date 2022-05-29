50dfd32b3ac9a2766877267f177ceb69b7335134
Doubleneck <antti.vuorenmaa@kolumbus.fi>
2022-05-29 19:33:18 +0300

    teht 1.7

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -5,7 +5,9 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
- 
+  const all = bad+neutral+good
+  const average = (good-bad)/all
+  const positive = 100*good/all
   return (
     <div>
       <h1>give feedback</h1>
@@ -16,6 +18,9 @@ const App = () => {
       <div>good {good}</div>
       <div>neutral {neutral}</div>
       <div>bad {bad}</div>
+      <div>all {all}</div>
+      <div>average {average}</div>
+      <div>positive {positive} %</div>
     </div>
   )
 }
