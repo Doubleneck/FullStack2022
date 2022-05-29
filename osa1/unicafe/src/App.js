04b9b7f4729c8b9c1d711b66db6edfd5011b6c56
Doubleneck <antti.vuorenmaa@kolumbus.fi>
2022-05-29 21:36:39 +0300

    harj 1.10

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -14,27 +14,55 @@ const Statistics = ({ good, neutral, bad }) => {
   return ( 
     <div>
       <h1>statistics</h1>
-      <div>good {good}</div>
-      <div>neutral {neutral}</div>
-      <div>bad {bad}</div>
-      <div>all {all}</div>
-      <div>average {average}</div>
-      <div>positive {positive} %</div>
+      <StatisticLine text="good" value = {good} />
+      <StatisticLine text="neutral" value = {neutral} />
+      <StatisticLine text="bad" value = {bad} />
+      <StatisticLine text="all" value = {all} />
+      <StatisticLine text="average" value = {average} />
+      <StatisticLine text="positive" value = {positive + ' %'} />
     </div>   
+  )
+}
+
+const StatisticLine  = ({text,value}) => {
+  return (
+    <div>
+      {text} {value}
+    </div>
+  )
+}
+
+const Button = (props) => {
+  return (
+    <button onClick={props.handleClick}>
+      {props.text}
+    </button>
   )
 }
 const App = () => {
   // tallenna napit omaan tilaansa
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const [ good, setGood] = useState(0)
+  const [ neutral, setNeutral] = useState(0)
+  const [ bad, setBad] = useState(0)
+  const addGood = () => setGood(good + 1)
+  const addNeutral = () => setNeutral(neutral + 1)
+  const addBad = () => setBad(bad + 1)
   
   return (
     <div>
       <h1>give feedback</h1>
-      <button onClick={() => setGood(good + 1)}>good</button>
-      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-      <button onClick={() => setBad(bad + 1)}>bad</button>
+      <Button
+        handleClick={addGood}
+        text='good'
+       /> 
+      <Button
+        handleClick={addNeutral}
+        text='neutral'
+       />  
+      <Button
+        handleClick={addBad}
+        text='bad'
+       />  
       <Statistics good={good} neutral={neutral} bad={bad}/>
     </div>
   )
