ae24e9d4ea0a6eb997b0d315aa32255b4bb143b8
Doubleneck <antti.vuorenmaa@kolumbus.fi>
2022-05-30 16:29:41 +0300

    teht 1.11 fix table

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -13,37 +13,37 @@ const Statistics = ({ good, neutral, bad }) => {
   return ( 
     <table>
     <tr>
-      <td>good</td>
+      <td><StatisticLine text = "good" /></td>
       <td><StatisticLine value = {good} /></td>
     </tr>
     <tr>
-      <td>neutral</td>
+      <td><StatisticLine text = "neutral" /></td>
       <td><StatisticLine value = {neutral} /></td>
     </tr>
     <tr>
-      <td>bad</td>
+      <td><StatisticLine text = "bad" /></td>
       <td><StatisticLine value = {bad} /></td>
     </tr>
     <tr>
-      <td>all</td>
+      <td><StatisticLine text = "all" /></td>
       <td><StatisticLine value = {all} /></td>
     </tr>
     <tr>
-      <td>average</td>
+      <td><StatisticLine text = "average" /></td>
       <td><StatisticLine value = {average} /></td>
     </tr>
     <tr>
-      <td>positive</td>
-      <td><StatisticLine value = {positive} /></td>
+      <td><StatisticLine text = "positive" /></td>
+      <td><StatisticLine value = {positive + '%'} /></td>
     </tr>
   </table>
   )
 }
 
-const StatisticLine  = ({value}) => {
+const StatisticLine  = ({text,value}) => {
   return (
     <div>
-      {value}
+      {text}{value}
     </div>
   )
 }
