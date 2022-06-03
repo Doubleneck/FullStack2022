a6ec588d7f1b090bb361832584b993e42135a661
Doubleneck <antti.vuorenmaa@kolumbus.fi>
2022-06-03 20:30:58 +0300

    2.4: kurssitiedot step8 & 9 new

--- osa2/kurssitiedot/src/App.js
+++ osa2/kurssitiedot/src/App.js
@@ -43,19 +43,25 @@ const App = () => {
       ]
     }
   ]
-  const courseExercisesTotal = courses.map(course => course.parts.map(part => part.exercises).reduce((result,number)=> result+number)) 
-  
-  console.log(courseExercisesTotal )
-  
+  console.log(courses)
   return (
     <div>
-      {courses.map(course => <ul key={course.id} ><h1>{course.name}</h1> 
-      {course.parts.map(part => <li key={part.id} style={{height:30,listStyleType : "none"}}>{part.name} {part.exercises}</li>)}
-       <li key={course.id} style={{height:30,listStyleType : "none",fontWeight: "bold"}}>total of {courseExercisesTotal[course.id-1]} exercises</li>
-      </ul>)}
+      {courses.map(course => <ul key={course.id}> <Course course={course}/></ul>)}
     </div>
   )
 }
+const Course = ({course}) => {
+  
+  const totalExercises = course.parts.map(part => part.exercises).reduce((result,number)=> result+number)
+  return(
+    <div>
+      <h1>{course.name}</h1>
+      {course.parts.map(part => <li key={part.id} style={{height:30,listStyleType : "none"}}>{part.name} {part.exercises}</li>)}
+      <li key={course.id} style={{height:30,listStyleType : "none",fontWeight: "bold"}}>total of {totalExercises} exercises</li>
+    </div>
+  )
+}
+
 
 
 export default App
