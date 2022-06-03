5b2c7a01822e56650818854da8ff6ee8771fab8f
Doubleneck <antti.vuorenmaa@kolumbus.fi>
2022-06-03 19:46:08 +0300

    2.4: kurssitiedot step8&9

--- osa2/kurssitiedot/src/App.js
+++ osa2/kurssitiedot/src/App.js
@@ -1,48 +1,61 @@
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    id: 1,
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      }
-    ]
-  }
-
-const Course = ({course}) => {
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
+  const courseExercisesTotal = courses.map(course => course.parts.map(part => part.exercises).reduce((result,number)=> result+number)) 
   
-  const parts = course.parts
-  const exercises = parts.map(part=> part.exercises)
-  const sumExercises = exercises.reduce((total, currentValue) => total = total + currentValue,0)
+  console.log(courseExercisesTotal )
   
-console.log(sumExercises);  // 600
-  console.log(exercises)
-  return(
-    <div>
-    <h1 key={course.id}>{course.name}</h1>
-   
-    {parts.map(part => <li key={part.id} style={{ lineHeight: 2, listStyleType: "none" }}>{part.name} {part.exercises}{'\n'}</li>)}
-    <p><b>total of {sumExercises} exercises</b></p>
-    </div>
-  )
-}
-
   return (
     <div>
-      <Course course={course} />
+      {courses.map(course => <ul key={course.id} ><h1>{course.name}</h1> 
+      {course.parts.map(part => <li key={part.id} style={{height:30,listStyleType : "none"}}>{part.name} {part.exercises}</li>)}
+       <li key={course.id} style={{height:30,listStyleType : "none",fontWeight: "bold"}}>total of {courseExercisesTotal[course.id-1]} exercises</li>
+      </ul>)}
     </div>
   )
 }
+
+
 export default App
