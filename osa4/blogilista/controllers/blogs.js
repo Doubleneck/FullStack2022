976e8ab057128db83571f0374bf30d6a27f9cf92
Doubleneck <antti.vuorenmaa@kolumbus.fi>
2022-09-16 19:35:51 +0300

    teht 4.15-4.20

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -1,15 +1,33 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
-
+/* const tokenExtractor = require('../utils/middleware') */
+const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+/* const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+} */
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})  
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 }) 
   response.json(blogs)
 })
 
 blogsRouter.post('/', async (request, response) => {
   let body = request.body
+  
+  const token = request.token //getTokenFrom(request)
+  /*  const token = tokenExtractor(request) */
 
+  console.log('token: ', token)
+  const decodedToken = jwt.verify(token, process.env.SECRET)
+  if (!token || !decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
   if (body.likes === undefined){
     body.likes = 0
   }
@@ -19,9 +37,12 @@ blogsRouter.post('/', async (request, response) => {
       title: body.title,
       author: body.author,
       url: body.url,
-      likes: body.likes
+      likes: body.likes,
+      user: user.id
     })
     const savedBlog = await blog.save()
+    user.blogs = user.blogs.concat(savedBlog.id)
+    await user.save()
     response.status(201).json(savedBlog)
    
   } else {
--- osa4/blogilista/utils/middleware.js
+++ osa4/blogilista/utils/middleware.js
@@ -8,9 +8,11 @@ const requestLogger = (request, response, next) => {
   next()
 }
 
+
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
+
 
 const errorHandler = (error, request, response, next) => {
   logger.error(error.message)
@@ -19,13 +21,29 @@ const errorHandler = (error, request, response, next) => {
     return response.status(400).send({ error: 'malformatted id' })
   } else if (error.name === 'ValidationError') {
     return response.status(400).json({ error: error.message })
+  } else if (error.name === 'JsonWebTokenError') {
+    return response.status(401).json({ error: 'invalid token' })
+  } else if (error.name === 'TokenExpiredError') {
+    return response.status(401).json({
+      error: 'token expired'
+    })
   }
-
   next(error)
+}
+
+const tokenExtractor = (request, response, next) => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    request.token = authorization.substring(7)
+  } else {
+    request.token = null
+  }
+  next()
 }
 
 module.exports = {
   requestLogger,
   unknownEndpoint,
-  errorHandler
+  errorHandler,
+  tokenExtractor
 }
