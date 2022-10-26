716eaf992eaafeb26005370106a1b3927bc6559e
Doubleneck <antti.vuorenmaa@kolumbus.fi>
2022-10-27 01:36:00 +0300

    teht 6.3-6.6

---
+++ osa6/redux-anecdotes-main/src/App.js
@@ -0,0 +1,40 @@
+import { useSelector, useDispatch } from 'react-redux'
+import { toggleVoteOf,createAnecdote  }from './reducers/anecdoteReducer'
+
+const App = () => {
+  const anecdotesFromSelector = useSelector(state => state)
+  const anecdotes = anecdotesFromSelector.sort((a, b) => b.votes - a.votes)
+  const dispatch = useDispatch()
+
+  const vote = (id) => {
+    dispatch(toggleVoteOf(id))
+  }
+
+  const create = (anecdote) => {
+    dispatch(createAnecdote(anecdote))
+  } 
+
+  return (
+    <div>
+      <h2>Anecdotes</h2>
+      {anecdotes.map(anecdote =>
+        <div key={anecdote.id}>
+          <div>
+            {anecdote.content}
+          </div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => vote(anecdote.id)}>vote</button>
+          </div>
+        </div>
+      )}
+      <h2>create new</h2>
+      <form onSubmit={create}>
+        <div><input name="anecdote"/></div>
+        <button type="submit">create</button>
+      </form>
+    </div>
+  )
+}
+
+export default App
