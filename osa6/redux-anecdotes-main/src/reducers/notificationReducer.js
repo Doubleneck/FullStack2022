d5bb38668f5a859bb5c6e959758e3fd3d4da69ce
Doubleneck <antti.vuorenmaa@kolumbus.fi>
2022-11-08 21:08:53 +0200

    fix bug in notificationReducer osa6, redux-anecdotes

--- osa6/redux-anecdotes-main/src/reducers/notificationReducer.js
+++ osa6/redux-anecdotes-main/src/reducers/notificationReducer.js
@@ -1,45 +1,41 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = {}
+const initialState = {};
 
 const notificationSlice = createSlice({
-    name: 'notification',
-    initialState,
-    reducers: {
-
-      createNotification (state, action) {
-        if (state.timeout){
-          console.log('timeout löytyy:',state.timeout)
-          clearTimeout(state.timeout)
-        } 
-        state.content = action.payload.content
-        state.timeout = action.payload.timeoutID
-        return state
-      },
-      clearNotification (state, action) {
-        
-        console.log('clear')
-        return initialState
+  name: "notification",
+  initialState,
+  reducers: {
+    createNotification(state, action) {
+      if (state.timeout) {
+        console.log("timeout löytyy:", state.timeout);
+        clearTimeout(state.timeout);
       }
+      state.content = action.payload.content;
+      state.timeout = action.payload.timeoutID;
+      return state;
     },
-  })
+    clearNotification(state, action) {
+      console.log("clear");
+      return initialState;
+    },
+  },
+});
 
-  export const setNotification = (content,time) => {  
-        const ms = time * 1000
-        return async dispatch => {
-          
-          const timeoutID = setTimeout(() => {
-            dispatch(clearNotification())
-          }, ms)
-          const notification ={
-            content: content,
-            timeoutID, timeoutID
-          }
-          dispatch(createNotification(notification)) 
+export const setNotification = (content, time) => {
+  const ms = time * 1000;
+  return async (dispatch) => {
+    const timeoutID = setTimeout(() => {
+      dispatch(clearNotification());
+    }, ms);
+    const notification = {
+      content: content,
+      timeoutID: timeoutID,
+    };
+    dispatch(createNotification(notification));
+  };
+};
 
-        
-    }
-  } 
- 
-  export const { createNotification, voteNotification, clearNotification } = notificationSlice.actions
-  export default notificationSlice.reducer
+export const { createNotification, voteNotification, clearNotification } =
+  notificationSlice.actions;
+export default notificationSlice.reducer;
